957d1ae6657799ebfd5af047612ecbe7d5b0ec5c
zekeThree <Ezekielsopchak@outlook.com>
2025-10-01 18:08:47 -0400

    Add files via upload

---
+++ NeuralNetworkAnimation.py
@@ -0,0 +1,13 @@
+from manim import *
+ 
+class NeuralNetworkAnimation(Scene):
+    def construct(self):
+        inputNeuronList = []
+        numberOfInputNeurons = 5
+        for i in range(numberOfInputNeurons):
+            inputNeuronList.append(Circle(radius=.2,color=BLUE))
+        for i in range(len(inputNeuronList)):
+            inputNeuronList[i].shift(DOWN * i)
+            self.play(Create(inputNeuronList[i]))
+
+            
